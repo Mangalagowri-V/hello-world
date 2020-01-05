7756e5d980c3cda40cc5de1d9ab0dcc5b1c81134
Mangalagowri <manglagowri@gmail.com>
2020-01-05 16:03:04 -0600

    Selenium methods

---
+++ Methods/src/Methods.java
@@ -0,0 +1,15 @@
+
+public class Methods {
+
+	public void arith(int a, int b) {
+		System.out.println(a + b);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Methods m = new Methods();
+		m.arith(2, 8);
+
+	}
+
+}
